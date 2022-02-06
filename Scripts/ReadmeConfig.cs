40fbea40cfa4bd0d32edb0636efc1ab031516a7e
jamesveug <james.veugelaers@outlook.co.nz>
2022-02-07 12:03:56 +1300

    Added config to change where the generated file gets saved to

--- Scripts/ReadmeConfig.cs
+++ Scripts/ReadmeConfig.cs
@@ -30,6 +30,7 @@ namespace ReadmeMaker
         public bool CardSortAscending => Plugin.Instance.Config.Bind("Card Sorting", "Ascending Order", true, new ConfigDescription("True=Names will be ordered from A-Z, False=Z-A... etc", null, Array.Empty<object>())).Value;
         public EnergyCostType CostEnergyIconType => Plugin.Instance.Config.Bind("Card Cost", "Display Type", EnergyCostType.Manta, new ConfigDescription("Different icon styles", null, Array.Empty<object>())).Value;
         public int CostMinCollapseAmount => Plugin.Instance.Config.Bind("Card Cost", "Min Collapse Amount", 4, new ConfigDescription("Minimum amount before costs are shown as (icon)5 instead of (icon)(icon)...etc", null, Array.Empty<object>())).Value;
+        public string SavePath => Plugin.Instance.Config.Bind("Saving", "Path", "", new ConfigDescription("Where to save this location to. If blank will be same folder as ReadmeMaker.dll. See console for exact location after making a readme", null, Array.Empty<object>())).Value;
 
         public ReadmeConfig()
         {
