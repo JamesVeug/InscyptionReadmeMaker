efe6f00c5ca9f66a437accccba57d3598e35182c
julian-perge <julianperge@gmail.com>
2022-03-29 11:42:08 -0400

    Converted from for loop to foreach loop

--- Scripts/Sections/AllNewCards.cs
+++ Scripts/Sections/AllNewCards.cs
@@ -13,9 +13,8 @@ namespace JamesGames.ReadmeMaker.Sections
             List<CardInfo> allCards = new List<CardInfo>(CardManager.NewCards);
 
             HashSet<string> referencesCardNames = new HashSet<string>();
-            for (int i = 0; i < allCards.Count; i++)
+            foreach (var cardInfo in allCards)
             {
-                CardInfo cardInfo = allCards[i];
                 if (cardInfo.evolveParams != null && cardInfo.evolveParams != null)
                 {
                     referencesCardNames.Add(cardInfo.evolveParams.evolution.name);
